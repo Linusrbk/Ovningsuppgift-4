9978dc85988095952629e9dccb44cf3438966f8d
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-11-09 15:14:08 +0100

    Update 4,2.py

--- 4,2.py
+++ 4,2.py
@@ -1,4 +1,4 @@
-# gör en kod som beräknar kvadraten och kubiken på 1 till 12
-for i in range(1,13,1):# i är variabeln som börjar på 1 slutar på 13 och går ett steg upp varje loop
+# gör en kod som beräknar kvadraten och kubiken på 1 till 13
+for i in range(1,14,1):# i är variabeln som börjar på 1 slutar på 13 och går ett steg upp varje loop
     print(f' {i} talet {i**2} talet i kvadrat {i**3} talet i kubik' ) #använder en print som har en f string med i in det så att alla text kommer irad 
    
